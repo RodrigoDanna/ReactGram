88e229f06fbd51a1d645cf29ae29e181139eff34
Rodrigo Danna <rrdanna@gmail.com>
2022-09-17 14:42:02 -0300

    general configs

--- api/app.js
+++ api/app.js
@@ -7,11 +7,20 @@ const cors = require("cors");
 const port = process.env.PORT;
 const app = express();
 
-//config JSON and form data response
+//Config JSON and form data response
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-//routes
+//Cors
+app.use(cors({credentials: true, origin: process.env.ORIGIN}));
+
+//Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+//DB connection
+require(".config/db.js");
+
+//Routes
 const router = require("./routes/Router.js");
 app.use(router);
 
