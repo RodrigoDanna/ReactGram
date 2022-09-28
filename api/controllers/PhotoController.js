074044cad3c72c1aa4e7a744652c1b44e2049682
Rodrigo Danna <rrdanna@gmail.com>
2022-09-28 19:48:44 -0300

    get photo by id

--- api/controllers/PhotoController.js
+++ api/controllers/PhotoController.js
@@ -63,9 +63,22 @@ const getUserPhotos = async(req, res) => {
     return res.status(200).json(photos);
 }
 
+const getPhotoById = async(req, res) => {
+    const {id} = req.params;
+    const photo = await Photo.findById(mongoose.Types.ObjectId(id));
+
+    if(!photo){
+        res.status(404).json({errors: ["Photo not found."]});
+        return;
+    }
+
+    return res.status(200).json(photo);
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
     getAllPhotos,
-    getUserPhotos
+    getUserPhotos,
+    getPhotoById
 }
