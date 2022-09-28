40b0902c307cc31f696a667f77e3affd8fb6f5de
Rodrigo Danna <rrdanna@gmail.com>
2022-09-28 19:35:54 -0300

    delete photo

--- api/controllers/PhotoController.js
+++ api/controllers/PhotoController.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 const mongoose = require("mongoose");
 
 // Insert
-const insertPhoto = async(req, res) =>{
+const insertPhoto = async(req, res) => {
     const {title} = req.body
     const image = req.file.filename;
 
@@ -22,11 +22,35 @@ const insertPhoto = async(req, res) =>{
 
     if(!newPhoto){
         res.status(422).json({errors: ["An unexpected error has occurred. Please try again."]})
+        return;
     }
 
     res.status(201).json(newPhoto);
 }
 
+const deletePhoto = async(req, res) => {
+    const {id} = req.params;
+
+    const reqUser = req.user;
+
+    try {
+        const photo = await Photo.findById(mongoose.Types.ObjectId(id));
+
+        // Check if photo exists
+        if(!photo || !photo.userId.equals(reqUser._id)){
+            res.status(404).json({errors: ["Photo not found."]});
+            return;
+        }
+
+        await Photo.findByIdAndDelete(photo._id);
+        
+        res.status(200).json({id: photo._id, message: "Photo deleted successfully."});
+    } catch (error) {
+        res.status(404).json({errors: ["Photo not found."]});
+    }
+}
+
 module.exports = {
-    insertPhoto
+    insertPhoto,
+    deletePhoto
 }
