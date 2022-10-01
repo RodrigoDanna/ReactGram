23e9cfac22c07c3bcee409f4f141a7fbaf86e02a
Rodrigo Danna <rrdanna@gmail.com>
2022-10-01 10:59:41 -0300

    register html

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Auth.css";
 
+// Components
+import { Link } from 'react-router-dom';
+
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Register</div>
+    <div>
+      <h2>ReactGram</h2>
+
+      <p className="subtitle">Register now and be part of ReactGram</p>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm Password" />
+        <input type="submit" value="Register" />
+      </form>
+
+      <p>Already have an account? <Link to="/login">Click here.</Link></p>
+    </div>
   )
 }
 
